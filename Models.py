e027596501dae433e41c2328e0013ffdcbdc4920
Jannes <jannbruns@uni-osnabrueck.de>
2025-05-29 15:41:51 +0200

    run pre commits on code

--- Models.py
+++ Models.py
@@ -1,54 +1,57 @@
+import torch
+import torch.nn as nn
 
-import torch, torch.nn as nn
 
 class CNN(torch.nn.Module):
-  def __init__(self, numClasses):
-        super(CNN,self).__init__()
+    def __init__(self, numClasses):
+        super(CNN, self).__init__()
         self.features = nn.Sequential(
             nn.Conv2d(3, 16, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=2, stride=2),
             nn.Conv2d(16, 32, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2, stride=2)
+            nn.MaxPool2d(kernel_size=2, stride=2),
         )
         self.classifier = nn.Sequential(
-            nn.Linear(32 * 56 * 56, 128),
-            nn.ReLU(inplace=True),
-            nn.Linear(128, numClasses)
+            nn.Linear(32 * 56 * 56, 128), nn.ReLU(inplace=True), nn.Linear(128, numClasses)
         )
 
-  def count_params(self):
-    return sum([p.view(-1).shape[0] for p in self.parameters()])
+    def count_params(self):
+        return sum([p.view(-1).shape[0] for p in self.parameters()])
 
-  def forward(self, x):
+    def forward(self, x):
         x = self.features(x)
         x = x.view(x.size(0), -1)
         x = self.classifier(x)
         return x
+
 
 class ResNetCNN(torch.nn.Module):
-  def __init__(self, numClasses):
-        super(ResNetCNN,self).__init__()
-        self.conv1= nn.Conv2d(3, 16, kernel_size=3, padding=1),
+    def __init__(self, numClasses):
+        super(ResNetCNN, self).__init__()
+        self.conv1 = (nn.Conv2d(3, 16, kernel_size=3, padding=1),)
 
-  def count_params(self):
-    return sum([p.view(-1).shape[0] for p in self.parameters()])
+    def count_params(self):
+        return sum([p.view(-1).shape[0] for p in self.parameters()])
 
-  def forward(self, x):
+    def forward(self, x):
         x = self.features(x)
         x = x.view(x.size(0), -1)
         x = self.classifier(x)
         return x
-#Resnet from here: https://wandb.ai/amanarora/Written-Reports/reports/Understanding-ResNets-A-Deep-Dive-into-Residual-Networks-with-PyTorch--Vmlldzo1MDAxMTk5
-#Bottleneck Block
 
-#overfits as fuck
+
+# Resnet from here: https://wandb.ai/amanarora/Written-Reports/reports/Understanding-ResNets-A-Deep-Dive-into-Residual-Networks-with-PyTorch--Vmlldzo1MDAxMTk5
+# Bottleneck Block
+
+
+# overfits as fuck
 class ResBlock(torch.nn.Module):
     def __init__(self, numClasses):
         super().__init__()
-        in_channels= 3
-        out_channels =16
+        in_channels = 3
+        out_channels = 16
 
         self.bottlenck1 = nn.Sequential(
             nn.BatchNorm2d(in_channels),
@@ -56,8 +59,7 @@ class ResBlock(torch.nn.Module):
             nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=1, padding=0, bias=False),
             nn.BatchNorm2d(out_channels),
             nn.ReLU(inplace=True),
-            nn.Conv2d(out_channels, out_channels, kernel_size=3, 
-                               stride=1, padding=1, bias=False),
+            nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=False),
             nn.BatchNorm2d(out_channels),
             nn.ReLU(inplace=True),
             nn.Conv2d(out_channels, in_channels, kernel_size=1, stride=1, padding=0, bias=False),
@@ -69,25 +71,19 @@ class ResBlock(torch.nn.Module):
             nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=1, padding=0, bias=False),
             nn.BatchNorm2d(out_channels),
             nn.ReLU(inplace=True),
-            nn.Conv2d(out_channels, out_channels, kernel_size=3, 
-                               stride=1, padding=1, bias=False),
+            nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=False),
             nn.BatchNorm2d(out_channels),
             nn.ReLU(inplace=True),
             nn.Conv2d(out_channels, in_channels, kernel_size=1, stride=1, padding=0, bias=False),
         )
 
-
-
-        self.pool=  nn.MaxPool2d(kernel_size=2, stride=2)
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
         self.shortcut1 = nn.Sequential()
         self.shortcut2 = nn.Sequential()
 
-        self.classifier = nn.Sequential(
-        
-            nn.Linear(3072, numClasses)
-        )
-    def forward(self, x):
+        self.classifier = nn.Sequential(nn.Linear(3072, numClasses))
 
+    def forward(self, x):
         out = self.bottlenck1(x)
         out += self.shortcut1(x)
         out = self.bottlenck2(out)
--- resNetEx.py
+++ resNetEx.py
@@ -1,35 +1,24 @@
 import numpy as np
-import os
-import pandas as pd
-import torch, torch.nn as nn
-from torch.utils.data import TensorDataset, Dataset
-from torch.optim.lr_scheduler import StepLR
-import matplotlib.pyplot as plt
-import mnist1d
 import torch
-import torchvision
-from torchvision import transforms,datasets
-import torch.nn.functional as F
-import torch.optim as optim
-import DataloaderFlowerSet
+import torch.nn as nn
 import Models
 import DataLoaderFFSet
-#fck cuda
-device=  torch.device('cpu')
+from datetime import datetime
 
-image_batch =DataLoaderFFSet.train_dataloader_simple
+# fck cuda
+device = torch.device("cpu")
+
+image_batch = DataLoaderFFSet.train_dataloader_simple
+
+
 def weights_init(layer_in):
-  if isinstance(layer_in, nn.Linear):
-    nn.init.kaiming_uniform_(layer_in.weight)
-    layer_in.bias.data.fill_(0.0)
-
-
-
+    if isinstance(layer_in, nn.Linear):
+        nn.init.kaiming_uniform_(layer_in.weight)
+        layer_in.bias.data.fill_(0.0)
 
 
 model = Models.ResBlock(5)
 model.apply(weights_init)
-from datetime import datetime
 
 criterion = nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
@@ -44,25 +33,22 @@ train_accs = np.zeros(num_epochs)
 val_accs = np.zeros(num_epochs)
 
 for epoch in range(num_epochs):
-    model.train() 
+    model.train()
     t0 = datetime.now()
-    
+
     train_loss = []
     val_loss = []
     n_correct_train = 0
     n_total_train = 0
 
-    
     for images, labels in DataLoaderFFSet.train_dataloader_simple:
-
         images = images.to(device)
         labels = labels.to(device)
 
-        
         optimizer.zero_grad()
- 
+
         y_pred = model(images)
-        loss = criterion(y_pred, labels)  
+        loss = criterion(y_pred, labels)
 
         loss.backward()
         optimizer.step()
@@ -79,10 +65,10 @@ for epoch in range(num_epochs):
     train_accs[epoch] = n_correct_train / n_total_train
     print(train_loss)
     # Validation phase
-    model.eval()  
+    model.eval()
     n_correct_val = 0
     n_total_val = 0
-    with torch.no_grad():  
+    with torch.no_grad():
         for images, labels in DataLoaderFFSet.test_dataloader_simple:
             images = images.to(device)
             labels = labels.to(device)
@@ -104,11 +90,12 @@ for epoch in range(num_epochs):
     duration = datetime.now() - t0
 
     # Print the metrics for the current epoch
-    print(f'Epoch [{epoch+1}/{num_epochs}] - '
-          f'Train Loss: {train_loss:.4f}, Train Accuracy: {train_accs[epoch]:.4f} | '
-      
-         f'Val Loss: {val_loss:.4f}, Val Accuracy: {val_accs[epoch]:.4f} | '
-          f'Duration: {duration}')
+    print(
+        f"Epoch [{epoch + 1}/{num_epochs}] - "
+        f"Train Loss: {train_loss:.4f}, Train Accuracy: {train_accs[epoch]:.4f} | "
+        f"Val Loss: {val_loss:.4f}, Val Accuracy: {val_accs[epoch]:.4f} | "
+        f"Duration: {duration}"
+    )
 
 # Optionally, save the model after training
 torch.save(model.state_dict(), "flower_classification_model.pth")
